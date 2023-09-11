55205811065dbdd240bdf78d022023057609c494
YE SL CHO <skyblueys123@gmail.com>
2023-09-11 16:58:17 +0900

    수업자료 업로드

---
+++ React/20230911/componentlist/src/Components/Hompage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hompage() {
+    return (
+        <main>
+            <h1>홈페이지에 오신 것을 환영합니다!</h1>
+            <button>로그아웃</button>
+        </main>
+    )
+}
