aea13f6d1a5212f07226d1d20521b93af9fce8e4
YE SL CHO <skyblueys123@gmail.com>
2023-09-13 22:05:21 +0900

    수업자료 업로드

---
+++ React/20230913/styled_component/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import ReactDOM from 'react-dom'; //구버전
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import App1 from "./App1";
+import App2 from "./App2";
+import App3 from "./App3";
+import App4 from "./App4";
+import App5 from "./App5";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App4 />);
