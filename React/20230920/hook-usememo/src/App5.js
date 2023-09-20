3446670bca1a117b9396bd2d91385920c9aa6f2b
YE SL CHO <skyblueys123@gmail.com>
2023-09-20 17:44:56 +0900

    수업자료 업로드

---
+++ React/20230920/hook-usememo/src/App5.js
@@ -0,0 +1,13 @@
+import { useContext, createContext } from "react";
+import HelloLicat from "./components/HelloLicat";
+
+// const UserInfo = createContext({ name: "gary", id: "garyIsFree" });
+
+const App5 = () => {
+    return (
+        <HelloLicat />
+    );
+};
+
+
+export default App5;
