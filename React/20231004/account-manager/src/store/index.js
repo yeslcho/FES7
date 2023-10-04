d3d6685c2f931cdb975e33e159ae941ec4076260
yeslcho <skyblueys123@gmail.com>
2023-10-04 21:26:18 +0900

    수업자료 업로드

---
+++ React/20231004/account-manager/src/store/index.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import expenseSlice from './expense/expense-slice';
+
+
+const store = configureStore({
+    reducer: {
+        expense: expenseSlice
+    }
+});
+
+export default store;
--- TDD/color-button/src/App.test.js
+++ TDD/color-button/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const button = screen.getByRole('button', { name: 'change to blue!' })
+  expect(button).toHaveStyle({ backgroundColor: 'red' })
+
+  fireEvent.click(button);
+  expect(button.textContent).toBe('change to red!');
 });
